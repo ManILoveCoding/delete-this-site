a3449d7131be6a24ac3e68be351142a85355577d
teaganconnon <teagan@ucsb.edu>
2021-01-17 06:29:03 -0800

    paypal time baby

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+
+function Product({ product }) {
+  const [paidFor, setPaidFor] = useState(false);
+  const [error, setError] = useState(null);
+  const paypalRef = useRef();
+
+  useEffect(() => {
+    window.paypal
+      .Buttons({
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                description: product.description,
+                amount: {
+                  currency_code: 'USD',
+                  value: product.price,
+                },
+              },
+            ],
+          });
+        },
+        onApprove: async (data, actions) => {
+          const order = await actions.order.capture();
+          setPaidFor(true);
+          console.log(order);
+        },
+        onError: err => {
+          setError(err);
+          console.error(err);
+        },
+      })
+      .render(paypalRef.current);
+  }, [product.description, product.price]);
+
+  if (paidFor) {
+    return (
+      <div>
+        <h1>Congrats, you just bought {product.name}!</h1>
+        {/* <img alt={product.description} src={gif} /> */}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {error && <div>Uh oh, an error occurred! {error.message}</div>}
+      <h1>
+        {product.description} for ${product.price}
+      </h1>
+      <div ref={paypalRef} />
+    </div>
+  );
+}
 
 function App() {
+  const product = {
+    price: 1.00,
+    name: 'delete',
+    description: 'delete this site',
+    //image: chair,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Product product={product} />
     </div>
   );
 }
